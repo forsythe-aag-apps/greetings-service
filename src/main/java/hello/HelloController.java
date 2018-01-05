f0c6dad277cc2c1061f722f9d2222c0d31bdc5c9
Alexander Kalinovski <a.kalinovski@gmail.com>
2018-01-05 18:31:45 +0300

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,9 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
+    public static String foo = "";
+    
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
     
+    public Object clone() {
+       return null;
+    }
+    
+    public int compareTo(Object value) {
+        if (value == null) {
+            return Integer.MIN_VALUE;
+        }
+        
+        return 0;
+    }
 }
