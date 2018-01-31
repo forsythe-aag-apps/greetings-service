6c120d6898d324cdb43196deda946bdabd5b0346
akalinovski <a.kalinovski@gmail.com>
2018-01-31 17:10:04 +0300

    Changed UI and output

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,16 +1,37 @@
 package hello;
 
 import io.micrometer.core.annotation.Timed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.MessageFormat;
 
 @RestController
 @Timed
 public class HelloController {
-    
-    @RequestMapping("/")
+
+    @Value("${templates.index}")
+    private String indexTemplate;
+
+    @Autowired
+    Environment environment;
+
     @Timed
+    @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        RestTemplate template = new RestTemplate();
+        String port = environment.getProperty("local.server.port");
+        String name = template.getForObject(String.format("http://localhost:%s/user", port), String.class);
+        return MessageFormat.format(indexTemplate, name);
+    }
+
+    @Timed
+    @RequestMapping("/user")
+    public String getUser() {
+        return "John Doe";
     }
 }
